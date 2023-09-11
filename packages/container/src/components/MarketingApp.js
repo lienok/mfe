34633d4ef4362abd17a6e70a33e3107cea8d5b5d
Weiss Lenka <Lenka.Weiss@acd.local>
2023-09-11 10:20:29 +0200

    add memory router & communication from marketing to container

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -1,11 +1,20 @@
 import { mount as marketingPagesMount } from 'marketing/MarketingPages'
 import React, { useRef, useEffect } from 'react';
+import { useHistory } from "react-router-dom";
 
 export default () => {
     const ref = useRef(null);
+    const history = useHistory();
 
     useEffect(() => {
-        marketingPagesMount(ref.current);
+        marketingPagesMount(ref.current, {
+            onNavigate: ({ pathname: nextPathname }) => {
+                const { pathname } = history.location;
+                if (pathname !== nextPathname) {
+                    history.push(nextPathname);
+                }
+            },
+        });
     }, []);
 
     return <dif ref={ref} />
